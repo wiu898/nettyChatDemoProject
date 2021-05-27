a397f904a187abe3b4fa9e14e8cb3362327df5da
Apple0Juice <1033489566@qq.com>
2021-05-27 20:34:01 +0800

    1

--- src/main/java/com/netty/chat/processor/MsgProcessor.java
+++ src/main/java/com/netty/chat/processor/MsgProcessor.java
@@ -1,9 +1,17 @@
 package com.netty.chat.processor;
 
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
 /**
  * 主要用于自定义协议内容的逻辑处理
  *
  * @author lichao chao.li07@hand-china.com 5/27/21 6:05 PM
  */
 public class MsgProcessor {
+
+    //记录在线用户
+    private static ChannelGroup onlineUsers = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
 }
